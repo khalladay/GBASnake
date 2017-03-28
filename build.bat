a42f8af45a4fc9fe8c4d34e0bbaba093f99696d0
khalladay <k.mj.halladay@gmail.com>
2017-03-27 22:33:13 -0500

    removing unused batch file

--- build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-arm-none-eabi-gcc -c code/main.cpp -mthumb-interwork -mthumb -O2 -o main.o
-arm-none-eabi-gcc main.o -mthumb-interwork -mthumb -specs=gba.specs -o main.elf
-arm-none-eabi-objcopy -v -O binary main.elf main.gba
-gbafix main.gba
